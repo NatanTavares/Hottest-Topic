4983a183c7ebf31d4dbbe4639d8795106879b590
NatanTavares <nattantavares.s15@gmail.com>
2022-06-19 23:22:08 -0300

    feat: add prisma and basic endpoint for voting

--- src/services/router.trpc.ts
+++ src/services/router.trpc.ts
@@ -1,19 +1,35 @@
 import * as trpc from "@trpc/server";
 import { PokemonClient } from "pokenode-ts";
+import { prisma } from "services/prisma";
 import { z } from "zod";
 
-export const appRouter = trpc.router().query("get-pokemon-by-id", {
-  input: z.object({
-    id: z.number(),
-  }),
+export const appRouter = trpc
+  .router()
+  .query("get-pokemon-by-id", {
+    input: z.object({
+      id: z.number(),
+    }),
 
-  async resolve({ input: { id } }) {
-    const api = new PokemonClient();
-    const pokemon = await api.getPokemonById(id);
+    async resolve({ input: { id } }) {
+      const api = new PokemonClient();
+      const pokemon = await api.getPokemonById(id);
 
-    return { name: pokemon.name, sprites: pokemon.sprites };
-  },
-});
+      return { name: pokemon.name, sprites: pokemon.sprites };
+    },
+  })
+  .mutation("cast-vote", {
+    input: z.object({
+      votedFor: z.number(),
+      votedAgainst: z.number(),
+    }),
+    async resolve({ input }) {
+      const vote = await prisma.vote.create({
+        data: input,
+      });
+
+      return { success: true, vote };
+    },
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
