1bbde9c48ac645b1792e02cd612bfc00f445c565
NatanTavares <nattantavares.s15@gmail.com>
2022-06-20 08:39:35 -0300

    refactor: Image optimization and caching with

--- src/components/optionToVote.tsx
+++ src/components/optionToVote.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Pokemon } from "types/types";
 import { Button } from "./button";
 
@@ -16,10 +17,14 @@ export const OptionToVote: React.FC<OptionToVoteProps> = ({
     <div className="flex flex-col gap-2">
       <div className="w-32 h-32 bg-gray-700 rounded hover:brightness-90 delay-75 duration-100">
         {sprites?.front_default && (
-          <img
+          <Image
             src={sprites.front_default}
-            alt="first option"
+            alt={name}
             className="w-full"
+            width={256}
+            height={256}
+            layout="fixed"
+            priority
           />
         )}
       </div>
